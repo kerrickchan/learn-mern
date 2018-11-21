f8707a00c854cc04092b2de2b8f27e9d0e1d199b
Eric <ericspi@outlook.com>
2018-11-21 23:04:02 +0800

    add basic routes file

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://eric:Abcd1234@ds163119.mlab.com:63119/mern"
+}
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users.js');
+const profile = require('./routes/api/profile.js');
+const posts = require('./routes/api/posts.js');
 
 const app = express();
 
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDb connected"))
+    .catch((err) => console.log(err));
+
 app.get('/', (req, res) => res.send("Hello World"));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
