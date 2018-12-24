f13df5f6969ea90ccdd1b4dc53230f0cf21a82ef
Eric.Chan <eric.chan@tasteofasia.com.hk>
2018-12-24 18:02:12 +0800

    add basic post create route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = requrie("mongoose");
+const passport = require("passport");
+
+// Load validators
+const validatePostInput = require("../../validation/post");
+
+// Load models
+const Post = require("../../models/Post");
 
 /**
  * @route   GET api/posts/test
@@ -7,5 +15,28 @@ const router = express.Router();
  * @access  Public
  */
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
+
+/**
+ * @route   POST api/posts
+ * @desc    Create comment route
+ * @access  Public
+ */
+router.post("/", passport.authenticate("jwt", {session: false}), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    })
+
+    newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
