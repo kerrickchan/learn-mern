13ebf5c2d405958f0975311687aa03bf2e840e4a
Eric Chan <ericspi@outlook.com>
2018-12-26 19:45:42 +0800

    update models format

--- models/Post.js
+++ models/Post.js
@@ -3,54 +3,25 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const PostSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String
-    },
-    avatar: {
-        type: String
-    },
+    user: {type: Schema.Types.ObjectId, ref: 'users'},
+    text: {type: String, required: true},
+    name: {type: String},
+    avatar: {type: String},
     likes: [
         {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
+            user: {type: Schema.Types.ObjectId, ref: 'users'}
         }
     ],
     comments: [
         {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
+            user: {type: Schema.Types.ObjectId, ref: 'users'},
+            text: {type: String, required: true},
+            name: {type: String},
+            avatar: {type: String},
+            date: {type: Date, default: Date.now}
         }
     ],
-    date: {
-        type: Date,
-        default: Date.now
-    }
+    date: {type: Date, default: Date.now}
 })
 
 module.exports = Post =  mongoose.model('post', PostSchema);
