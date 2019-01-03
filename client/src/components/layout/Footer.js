b26116b2a40bbff390dafdfbeab37647f1953a52
Eric Chan <ericspi@outlook.com>
2019-01-03 22:45:51 +0800

    add router setup

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className="bg-dark text-white mt-5 p-4 text-center">
-                Copyright &copy; {new Date().getFullYear} DevConnector
+                Copyright &copy; {new Date().getFullYear()} DevConnector
             </footer>
         )
     }
