fbc1c7d147d7d9718661b8819f2232622cc29fca
Eric Chan <ericspi@outlook.com>
2019-01-05 18:46:00 +0800

    fix problem of errors feedback

--- validation/register.js
+++ validation/register.js
@@ -21,7 +21,7 @@ module.exports = function validateRegisterInput(data) {
         errors.email = "Email field is required";
     }
 
-    if(!Validator.isEmail(data.email)) {
+    if(!Validator.isEmpty(data.email) && !Validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
     }
 
@@ -29,7 +29,7 @@ module.exports = function validateRegisterInput(data) {
         errors.password = "Password field is required";
     }
 
-    if(!Validator.isLength(data.password, {min: 6, max: 30})) {
+    if(!Validator.isEmpty(data.password) && !Validator.isLength(data.password, {min: 6, max: 30})) {
         errors.password = "Password must be at least 6 characters";
     }
 
