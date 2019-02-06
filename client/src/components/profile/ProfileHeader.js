5515e0c25d4dfc4dbac322c6f30270aef40c0cef
Eric Chan <ericspi@outlook.com>
2019-02-06 20:39:38 +0800

    finish profile header

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import isEmpty from '../../validation/is-empty'
 
 class ProfileHeader extends Component {
   static propTypes = {
-    prop: PropTypes
+    profile: PropTypes.object.isRequired
   }
 
   render() {
+    const {profile} = this.props;
+
     return (
-      <div>
-          <h1>TODO: PROFILE HEADER</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb-3">
+            <div class="row">
+              <div className="col-4 col-md-3 m-auto">
+                <img className="rounded-circle" alt={profile.user.name} src={profile.user.avatar} />
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="display-4 text-center">{profile.user.name}</h1>
+              <p className="lead text-center">{profile.status} {isEmpty(profile.company) ? null : (<span>at {profile.company}</span>)}</p>
+              <p>{profile.location}</p>
+              <p>
+                {isEmpty(profile.website) ? null : (<a className="text-white p-2" href={profile.website} target="_blank" rel="noopener noreferrer"><i className="fab fa-globe fa-2x"></i></a>)}
+                {isEmpty(profile.social.twitter) ? null : (<a className="text-white p-2" href={profile.social.twitter} target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter fa-2x"></i></a>)}
+                {isEmpty(profile.social.facebook) ? null : (<a className="text-white p-2" href={profile.social.facebook} target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook fa-2x"></i></a>)}
+                {isEmpty(profile.social.linkedin) ? null : (<a className="text-white p-2" href={profile.social.linkedin} target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>)}
+                {isEmpty(profile.social.instagram) ? null : (<a className="text-white p-2" href={profile.social.instagram} target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram fa-2x"></i></a>)}
+                {isEmpty(profile.social.github) ? null : (<a className="text-white p-2" href={profile.social.github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x"></i></a>)}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
